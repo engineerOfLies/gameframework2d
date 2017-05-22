10ac5629974a5155775276937048264d9e26e709
DJ Kehoe <donald.j.kehoe@gmail.com>
2017-05-21 21:37:52 -0400

    adding color support

---
+++ include/gf2d_color.h
@@ -0,0 +1,131 @@
+#ifndef __GF2D_COLOR_H__
+#define __GF2D_COLOR_H__
+
+#include <SDL.h>
+#include "gf2d_vector.h"
+
+typedef enum
+{
+    CT_RGBAf,   /**<RGBA in floating point 0-1*/
+    CT_RGBA8,   /**<RGBA in 8 bit int 0-255*/
+    CT_HSL,     /**<Hue,Saturation and Lightness*/
+    CT_HEX      /**<Hex code 0xFFFFFF*/
+}ColorType;
+
+typedef struct
+{
+    float r;    /**<red value from 0-1, or the HUE*/
+    float g;    /**<green value or Saturation*/
+    float b;    /**<blue value or Lightness*/
+    float a;    /**<alpha, or transparency from 0 (clear) to opaque 1*/
+    ColorType ct;   /**<how this structure is used*/
+}Color;
+
+/**
+ * @brief create and return a color from floating point RGBA values
+ * values over 1 are clamped to 1, values under 0 are clamped to zero
+ * @param r red value 0 - 1
+ * @param g green value 0 - 1
+ * @param b blue value 0 - 1
+ * @param a alpha value 0 - 1
+ * @returns a newly set color
+ */
+Color gf2d_color(float r,float g,float b,float a);
+/**
+ * @brief create and return a color from 8 bit integer RGBA values
+ * @param r red value 0 - 255
+ * @param g green value 0 - 255
+ * @param b blue value 0 - 255
+ * @param a alpha value 0 - 255
+ * @returns a newly set color
+ */
+Color gf2d_color8(Uint8 r,Uint8 g,Uint8 b,Uint8 a);
+/**
+ * @brief create and return a color from floating point RGBA values
+ * @param h hue value 0 - 1
+ * @param s saturation value 0 - 1
+ * @param l lightness value 0 - 1
+ * @param a alpha value 0 - 1
+ * @returns a newly set color
+ */
+Color gf2d_color_hsl(float h,float s,float l,float a);
+/**
+ * @brief create and return a color from floating point RGBA values
+ * @param hex the hex value of the color
+ * @returns a newly set color
+ */
+Color gf2d_color8(Uint32 hex);
+
+/**
+ * @brief convert a color to floating point representation
+ * @param color the color to convert
+ * @return a new color in the format CT_RGBAf
+ */
+Color gf2d_color_to_float(Color color);
+
+/**
+ * @brief convert a color to 8 bit integer representation
+ * @param color the color to convert
+ * @return a new color in the format CT_RGBA8
+ */
+Color gf2d_color_to_int8(Color color);
+
+/**
+ * @brief convert a color to Hue Saturation Lightness (and Alpha) representation
+ * @param color the color to convert
+ * @return a new color in the format CT_HSL
+ */
+Color gf2d_color_to_hsla(Color color);
+
+/**
+ * @brief convert a color to Hex representation
+ * @param color the color to convert
+ * @return a new color in the format CT_HEX
+ */
+Uint32 gf2d_color_to_hex(Color color);
+
+/**
+ * @brief convert a SDL color to gf2d floating point representation
+ * @param color the color to convert
+ * @return a new color in the format CT_RGBAf
+ */
+Color gf2d_color_from_sdl(SDL_Color color);
+
+/**
+ * @brief convert a gf2d color to SDL color format
+ * @param color the color to convert
+ * @return a new SDL_Color
+ */
+SDL_Color gf2d_color_to_sdl(Color color);
+
+/**
+ * @brief create a color from a vector where x = r, y = g, z = b, w = a
+ * @param vector the vector to convert
+ * @return a new color in the format CT_HSL
+ */
+Color gf2d_color_from_vector4(Vector4D vector);
+
+/**
+ * @brief convert a color to a vector where x = r, y = g, z = b, w = a
+ * @param color the color to convert
+ * @return a new color in the format CT_HSL
+ */
+Vector4D gf2d_color_to_vector4(Color color);
+
+/**
+ * @brief Get the Hue value of a color.  It may convert internally as needed.
+ * @param color the color to check
+ * @return the value of the Hue of the color (0-360)
+ */
+float gf2d_color_get_hue(Color color);
+
+/**
+ * @brief Sets the hue of the color provided.  Leaves format unchanged.  Saturation and Value are also unchanged
+ * if saturation is zero this may not show a change
+ * @param hue the hue to change to
+ * @param color the color to change
+ * @return a new color in the format CT_HSL
+ */
+void gf2d_color_set_hue(float hue,Color *color);
+
+ #endif
---
+++ src/gf2d_color.c
@@ -0,0 +1,313 @@
+#include "gf2d_color.h"
+
+
+Color gf2d_color(float r,float g,float b,float a)
+{
+    Color color;
+    color.r = MIN(MAX(r,0),1);
+    color.g = MIN(MAX(g,0),1);
+    color.b = MIN(MAX(b,0),1);
+    color.a = MIN(MAX(a,0),1);
+    color.ct = CT_RGBAf;
+    return color;
+}
+
+Color gf2d_color8(Uint8 r,Uint8 g,Uint8 b,Uint8 a)
+{
+    Color color;
+    color.r = (float)r;
+    color.g = (float)g;
+    color.b = (float)b;
+    color.a = (float)a;
+    color.ct = CT_RGBA8;
+    return color;
+}
+
+Color gf2d_color_hsl(float h,float s,float l,float a)
+{
+    Color color;
+    color.r = h;
+    while (color.r < 0)color.r+=360;
+    while (color.r >= 360)color.r-=360;
+    color.g = MIN(MAX(s,0),1);
+    color.b = MIN(MAX(l,0),1);
+    color.a = MIN(MAX(a,0),1);
+    color.ct = CT_HSL;
+    return color;
+}
+
+Color gf2d_color8(Uint32 hex)
+{
+    Color color;
+    color.r = (float)hex;
+    color.ct = CT_HEX;
+}
+
+Color gf2d_color_to_float(Color color)
+{
+    Color nc;
+    float C,X,m;
+    float factor = 1.0/255.0;
+    switch(color.ct)
+    {
+        default:
+        case CT_RGBAf:
+            return color;
+        case CT_RGBA8:
+            nc.r = color.r *factor;
+            nc.g = color.r *factor;
+            nc.b = color.r *factor;
+            nc.a = color.r *factor;
+            break;
+        case CT_HSL:
+            C = (1 - fabs(2*color.b -1)) * color.g;
+            X = C * (1 - fabs(color.r/60 % 2 - 1));
+            m = color.b - (C * 0.5);
+            if (color.r < 60)
+            {
+                nc.r = C+m;
+                nc.g = X+m;
+                nc.b = 0+m;
+            }
+            else if (color.r < 120)
+            {
+                nc.r = X+m;
+                nc.g = C+m;
+                nc.b = 0+m;
+            }
+            else if (color.r < 180)
+            {
+                nc.r = 0+m;
+                nc.g = C+m;
+                nc.b = X+m;
+            }
+            else if (color.r < 240)
+            {
+                nc.r = 0+m;
+                nc.g = X+m;
+                nc.b = C+m;
+            }
+            else if (color.r < 300)
+            {
+                nc.r = X+m;
+                nc.g = 0+m;
+                nc.b = C+m;
+            }
+            else
+            {
+                nc.r = C+m;
+                nc.g = 0+m;
+                nc.b = X+m;
+            }
+            break;
+        case CT_HEX:
+            color.r = ((float)(hex & 0xff000000)>>24)/0xff;
+            color.g = ((float)(hex & 0x00ff0000)>>16)/0xff;
+            color.b = ((float)(hex & 0x0000ff00)>>8)/0xff;
+            color.a = ((float)hex & 0x000000ff)/0xff;
+            break;
+    }
+    nc.ct = CT_RGBAf
+    return nc;
+}
+
+Color gf2d_color_to_int8(Color color)
+{
+    Color nc;
+    switch (color.ct)
+    {
+        case CT_HEX:
+            color.r = ((float)(hex & 0xff000000)>>24);
+            color.g = ((float)(hex & 0x00ff0000)>>16);
+            color.b = ((float)(hex & 0x0000ff00)>>8);
+            color.a = ((float)hex & 0x000000ff);
+        case CT_HSL:
+            C = (1 - fabs(2*color.b -1)) * color.g;
+            X = C * (1 - fabs(color.r/60 % 2 - 1));
+            m = color.b - (C * 0.5);
+            if (color.r < 60)
+            {
+                nc.r = C+m * 255;
+                nc.g = X+m * 255;
+                nc.b = 0+m * 255;
+            }
+            else if (color.r < 120)
+            {
+                nc.r = X+m * 255;
+                nc.g = C+m * 255;
+                nc.b = 0+m * 255;
+            }
+            else if (color.r < 180)
+            {
+                nc.r = 0+m * 255;
+                nc.g = C+m * 255;
+                nc.b = X+m * 255;
+            }
+            else if (color.r < 240)
+            {
+                nc.r = 0+m * 255;
+                nc.g = X+m * 255;
+                nc.b = C+m * 255;
+            }
+            else if (color.r < 300)
+            {
+                nc.r = X+m * 255;
+                nc.g = 0+m * 255;
+                nc.b = C+m * 255;
+            }
+            else
+            {
+                nc.r = C+m * 255;
+                nc.g = 0+m * 255;
+                nc.b = X+m * 255;
+            }
+            nc.a = color.a *255;
+            break;
+        case CT_RGBA8:
+            return color;
+        case CT_RGBAf:
+        default:
+            nc.r = color.r *255;
+            nc.g = color.g *255;
+            nc.b = color.b *255;
+            nc.a = color.a *255;
+            break;
+    }
+    nc.ct = CT_RGBA8;
+    return nc;
+}
+
+Color gf2d_color_to_hsla(Color color)
+{
+    Color nc;
+    float D,Cmin,Cmax,H,S,L;
+    if (color.ct == CT_HSL)
+    {
+        return color;
+    }
+    nc = gf2d_color_to_float(color);
+    Cmax = MAX(MAX(nc.r,nc.g),nc.b);
+    Cmin = MIN(MIN(nc.r,nc.g),nc.b);
+    D = Cmax - Cmin;
+    L = (Cmax + Cmin) * 0.5;
+    if (D == 0.0)
+    {
+        H = 0;
+        S = 0;
+    }
+    else
+    {
+        S = D / (1 - fabs(Cmax+Cmin-1));
+        if (Cmax == nc.r)
+        {
+            H = 60 * ((nc.g - nc.b)/D)%6;
+        }
+        else if (Cmax == nc.g)
+        {
+            H = 60 * (((nc.b - nc.r)/D)+2);
+        }
+        else
+        {
+            H = 60 * (((nc.r - nc.g)/D)+4);
+        }
+    }
+    nc.r = H;
+    nc.g = S;
+    nc.b = L;
+    nc.ct = CT_HSL;
+    return nc;
+}
+
+Uint32 gf2d_color_to_hex(Color color)
+{
+    Uint32 hex;
+    Uint32 r,g,b,a;
+    Color nc;
+    if (color.ct == CT_HEX)
+    {
+        return color.r;
+    }
+    nc = gf2d_color_to_int8(color);
+    r = (Uint32)(nc.r) << 24;
+    g = (Uint32)(nc.g) << 16;
+    b = (Uint32)(nc.b) << 8;
+    a = (Uint32)(nc.a);
+    hex = r | g | b | a;
+    return hex;
+}
+
+Color gf2d_color_from_vector4(Vector4D vector)
+{
+    Color color;
+    color.ct = CT_RGBAf;
+    color.r = vector.x;
+    color.g = vector.y;
+    color.b = vector.z;
+    color.a = vector.w;
+    return color;
+}
+
+Vector4D gf2d_color_to_vector4(Color color)
+{
+    Vector4D vector;
+    color = gf2d_color_to_float(color);
+    vector.x = color.r;
+    vector.y = color.g;
+    vector.z = color.b;
+    vector.w = color.a;
+    return vector;
+}
+
+float gf2d_color_get_hue(Color color)
+{
+    color = gf2d_color_to_hsla(color);
+    return color.r;
+}
+
+Color gf2d_color_from_sdl(SDL_Color color)
+{
+    Color nc;
+    float factor = 1.0/255.0;
+    nc.ct = CT_RGBAf;
+    nc.r = color.r *factor;
+    nc.g = color.r *factor;
+    nc.b = color.r *factor;
+    nc.a = color.r *factor;
+    return nc;
+}
+
+SDL_Color gf2d_color_to_sdl(Color color)
+{
+    SDL_Color nc;
+    color = gf2d_color_to_int8(color);
+    nc.r = (Uint8)color.r;
+    nc.g = (Uint8)color.g;
+    nc.b = (Uint8)color.b;
+    nc.a = (Uint8)color.a;
+    return nc;
+}
+
+void gf2d_color_set_hue(float hue,Color *color)
+{
+    Color temp;
+    if (color.ct == CT_HSL)
+    {
+        color.r = hue;
+        return;
+    }
+    temp = gf2d_color_to_hsla(*color);
+    temp.r = hue;
+    switch(color.ct)
+    {
+        case CT_HEX:
+            *color = gf2d_color_to_hex(temp);
+            return;
+        case CT_RGBA8:
+            *color = gf2d_color_to_int8(temp);
+            return;
+        case CT_RGBAf:
+            *color = gf2d_color_to_float(temp);
+    }
+}
+
+/*eol@eof*/
