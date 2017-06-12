adf0b26a811523e1d0cf27e5e4ad349712cd7cd4
DJ Kehoe <donald.j.kehoe@gmail.com>
2017-06-12 16:15:46 -0400

    font files in place

--- include/gf2d_text.h
+++ include/gf2d_text.h
@@ -27,7 +27,7 @@
  */
 
 #include "gf2d_types.h"
-
+#include "gf2d_color.h"
 /**
  * constant lenth character buffers.
  * These will be used when the convenience of GString is not needed
@@ -57,5 +57,28 @@ typedef char TextBlock[512];
 #define gf2d_block_cmp(a,b) (strncmp(a,b,GF2DTEXTLEN))
 #define gf2d_block_cpy(dst,src) (strncpy(dst,src,GF2DTEXTLEN))
 #define gf2d_block_clear(a)  (memset(a,0,sizeof(char)*GF2DTEXTLEN))
+
+typedef enum
+{
+    FT_Normal,
+    FT_Small,
+    FT_H1,
+    FT_H2,
+    FT_H3,
+    FT_H4,
+    FT_H5,
+    FT_H6,
+    FT_MAX
+}FontTypes;
+
+/**
+ * @brief initialized text drawing system
+ * @param configFile the file to load font information from
+ */
+void gf2d_text_init(char *configFile);
+
+void gf2d_text_draw_line_font(char *text,char *filename,Color color, Vector2D position);
+
+void gf2d_text_draw_line(char *text,FontTypes tag,Color color, Vector2D position);
 
 #endif
