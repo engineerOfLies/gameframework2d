d1a8e574dab7132fa836566f0ca318434f5f6ecc
DJ Kehoe <donald.j.kehoe@gmail.com>
2017-06-16 21:30:12 -0400

    standards compliance

--- include/gf2d_types.h
+++ include/gf2d_types.h
@@ -1,13 +1,16 @@
 #ifndef __GF2D_TYPES_H__
 #define __GF2D_TYPES_H__
 
+#include <stdlib.h>
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 #define GF2D_EPSILON   1e-6f
-
+#define GF2D_ROOT2  1.414213562
 #define GF2D_2PI 6.283185308
-
+#define GF2D_PI 3.141592654
+#define GF2D_HALF_PI 1.570796327
 /* conversion factor for converting from radians to degrees*/
 #define GF2D_RADTODEG  57.295779513082
 
