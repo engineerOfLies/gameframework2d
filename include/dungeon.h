7ee88173d229ef7af59806384e4bddb8619906f1
DJ Kehoe <donald.j.kehoe@gmail.com>
2017-09-28 15:51:18 -0400

    adding dungeon template

---
+++ include/dungeon.h
@@ -0,0 +1,18 @@
+#ifndef __DUNGEON_H__
+#define __DUNGEON_H__
+
+typedef struct
+{
+	int *tiles;
+	unsigned int width;
+	unsinged int height;
+}Dungeon;
+
+/**
+
+	tile x,y would be found at x + width *y;
+
+
+*/
+
+#endif
