a5ba48fd38076c03907cb3be9b3fb5d19897537d
DJ Kehoe <donald.j.kehoe@gmail.com>
2023-01-26 15:37:23 -0500

    today's in-class example

---
+++ include/entity.h
@@ -0,0 +1,61 @@
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+
+#include "gfc_types.h"
+#include "gfc_vector.h"
+
+#include "gf2d_sprite.h"
+
+typedef struct Entity_S
+{
+    Bool    _inuse;
+    Sprite *sprite;
+    float   frame;
+    Vector2D position;
+    Vector2D velocity;
+    Vector2D acceleration;
+    
+    void (*think)(struct Entity_S *self);
+}Entity;
+
+/**
+ * @brief initialize the internal manager for the entity system
+ * @note this will automatically queue up the close function for program exit
+ * @param max this is the maximum number of supported entities at a given time
+ */
+void entity_manager_init(Uint32 max);
+
+/**
+ * @brief alloate an initialize a new entity
+ * @return NULL if there are no entities left, an empty entity otherwise
+ */
+Entity *entity_new();
+
+/**
+ * @brief free a previously allocated entity
+ * @param ent the entity to free
+ */
+void entity_free(Entity *ent);
+
+/**
+ * @brief free all allocated entities
+ */
+void entity_free_all();
+
+/**
+ * @brief draw all active entities if they have graphics
+ */
+void entity_draw_all();
+
+/**
+ * @brief update all active entities
+ */
+void entity_update_all();
+
+/**
+ * @brief call all the think functions for the entities, if they have one
+ */
+void entity_think_all();
+
+
+#endif
---
+++ src/entity.c
@@ -0,0 +1,132 @@
+#include "simple_logger.h"
+#include "entity.h"
+
+typedef struct
+{
+    Uint32  entity_max;
+    Entity *entity_list;
+}EntityManager;
+
+static EntityManager entity_manager = {0};
+
+void entity_manager_close()
+{
+    entity_free_all();
+    if (entity_manager.entity_list)free(entity_manager.entity_list);
+    slog("entity system closed");
+}
+
+void entity_manager_init(Uint32 max)
+{
+    if (max <= 0)
+    {
+        slog("cannot intialize entity system: zero entities specified!");
+        return;
+    }
+    entity_manager.entity_list = gfc_allocate_array(sizeof(Entity),max);
+    if (!entity_manager.entity_list)
+    {
+        slog("failed to initialize entity system!");
+        return;
+    }
+    entity_manager.entity_max = max;
+    atexit(entity_manager_close);
+    slog("entity system initialized");
+}
+
+void entity_free_all()
+{
+    int i;
+    for (i = 0; i < entity_manager.entity_max;i++)
+    {
+        if (!entity_manager.entity_list[i]._inuse)continue;
+        entity_free(&entity_manager.entity_list[i]);
+    }
+}
+
+Entity *entity_new()
+{
+    int i;
+    for (i = 0; i < entity_manager.entity_max;i++)
+    {
+        if (entity_manager.entity_list[i]._inuse)continue;
+        entity_manager.entity_list[i]._inuse = 1;
+        return &entity_manager.entity_list[i];
+    }
+    return NULL;
+}
+
+void entity_free(Entity *ent)
+{
+    if (!ent)
+    {
+        slog("no entity provided");
+        return;
+    }
+    if (ent->sprite)gf2d_sprite_free(ent->sprite);
+    memset(ent,0,sizeof(Entity));
+}
+
+void entity_draw(Entity *ent)
+{
+    if (!ent)return;
+    if (ent->sprite)
+    {
+        gf2d_sprite_draw(
+            ent->sprite,
+            ent->position,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            (int)ent->frame);
+    }
+}
+
+void entity_draw_all()
+{
+    int i;
+    for (i = 0; i < entity_manager.entity_max;i++)
+    {
+        if (!entity_manager.entity_list[i]._inuse)continue;
+        entity_draw(&entity_manager.entity_list[i]);
+    }
+}
+
+void entity_update(Entity *ent)
+{
+    if (!ent)return;
+    ent->frame += 0.1;
+    if (ent->frame >= 16)ent->frame = 0;
+    vector2d_add(ent->position,ent->position,ent->velocity);
+}
+
+void entity_update_all()
+{
+    int i;
+    for (i = 0; i < entity_manager.entity_max;i++)
+    {
+        if (!entity_manager.entity_list[i]._inuse)continue;
+        entity_update(&entity_manager.entity_list[i]);
+    }
+}
+
+void entity_think(Entity *ent)
+{
+    if (!ent)return;
+    if (ent->think)ent->think(ent);
+}
+
+void entity_think_all()
+{
+    int i;
+    for (i = 0; i < entity_manager.entity_max;i++)
+    {
+        if (!entity_manager.entity_list[i]._inuse)continue;
+        entity_think(&entity_manager.entity_list[i]);
+    }
+}
+
+
+/*eol@eof*/
